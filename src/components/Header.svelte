474e81b8116b8f0fbff38d61db98b5e010ddb8e2
Steven A.J <johnsonstheory@gmail.com>
2023-11-15 17:13:46 -0700

    applying stashes

---
+++ src/components/Header.svelte
@@ -0,0 +1,14 @@
+<header>
+   <slot name="title" />
+   <slot name="options" />
+</header>
+
+<style lang="postcss">
+   header {
+      /* Container */
+      @apply relative flex flex-col md:flex-row items-center;
+
+      /* Spacing */
+      @apply md:justify-between my-6 p-4;
+   }
+</style>
