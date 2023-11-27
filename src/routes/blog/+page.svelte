13e72bf3a5d03bda1b93cd7eb0727d47489c30f0
Steven A.J <31742073+steven-aj@users.noreply.github.com>
2023-11-27 10:15:38 -0700

    Update +page.svelte

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -11,7 +11,7 @@
 
    // let drawer = getDrawerStore();
 
-   let { posts, tags } = data;
+   let { posts, tags, recent } = data;
    let ready = false;
 
    function openTagCloud() {
@@ -22,6 +22,7 @@
       Shell.showBackButton(false);
       Shell.setHero(false);
       setTimeout(() => (ready = true), 100);
+      console.log(tags);
    }
 
    beforeUpdate(init);
@@ -31,22 +32,38 @@
    <TagCloud {tags} on:select={() => drawer.close()} />
 </Drawer> -->
 
-<main class="container" in:fade>
+<h1 class="container">Blog</h1>
+<main class="container-fluid" in:fade>
+   <aside>
+      <button class="btn variant-filled-secondary" on:click={openTagCloud}
+         >Tag Cloud</button
+      >
+
+      <section id="tags">
+         <h2>Tag Cloud</h2>
+         <div class="wrapper">
+            {#each tags as tag}
+            <a class="primary badge" href={`/blog/tags/${tag}`}>{tag}</a>
+            {/each}
+         </div>
+      </section>
+
+      <section id="recent">
+         <h2>Recent Posts</h2>
+         <div class="wrapper">
+            {#each recent as post}
+            <a href={post.path}>{post.title}</a>
+            {/each}
+         </div>
+      </section>
+   </aside>
    {#if data}
       {#if posts.length}
-         <section class="container posts" transition:fade>
-            <header>
-               <h1>Blog</h1>
-               <button
-                  class="btn variant-filled-secondary"
-                  on:click={openTagCloud}>Tag Cloud</button
-               >
-            </header>
-            <div class="row">
-               {#each posts as post}
-                  <PostCard {post} />
-               {/each}
-            </div>
+
+         <section id="posts" class="container-fluid" transition:fade>
+            {#each posts as post}
+               <PostCard {post} />
+            {/each}
          </section>
       {:else}
          <EmptyNotice />
@@ -54,5 +71,48 @@
    {/if}
 </main>
 
-<style>
+<style lang="postcss">
+   main {
+      @apply flex flex-col lg:grid lg:grid-cols-12 gap-2;
+   }
+
+   h1 {
+      @apply text-center;
+   }
+
+   main aside {
+      @apply self-start block lg:sticky lg:top-24 lg:left-0 lg:col-span-2 px-4;
+   }
+
+   main aside button {
+      @apply lg:hidden;
+   }
+
+   main aside h2 {
+      @apply text-lg opacity-50;
+   }
+
+   main aside section#tags {
+      @apply items-center;
+   }
+
+   main aside section#tags .wrapper {
+      @apply flex flex-wrap gap-2 justify-center;
+   }
+
+   main aside section#tags .badge {
+      @apply hover:no-underline;
+   }
+
+   main aside section#recent {
+      @apply flex flex-col;
+   }
+
+   main aside section#recent .wrapper {
+      @apply flex flex-col;
+   }
+
+   section#posts {
+      @apply flex flex-col lg:grid lg:col-span-10 md:grid-cols-3 gap-4;
+   }
 </style>
