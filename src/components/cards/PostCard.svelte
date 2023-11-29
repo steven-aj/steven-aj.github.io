acc45ff8036e79a8b23def338d694a0041577542
Steven A.J <johnsonstheory@gmail.com>
2023-11-29 16:35:04 -0700

    re-designed postcard with additional effects

--- src/components/cards/PostCard.svelte
+++ src/components/cards/PostCard.svelte
@@ -3,25 +3,29 @@
 </script>
 
 <a title={post.title} class="card" href={post.path}>
-   <article>
-      <section style={`${post.cover ? `background-image: url(${post.cover});` : ''}`}>
+   <!-- <article> -->
+   <section
+      style={`${post.cover ? `background-image: url(${post.cover});` : ""}`}
+   >
+      <div class="backdrop">
          <h1>{post.title}</h1>
-      </section>
-   
-      <footer>
-         {#if post.tags}
-            <div class="tags">
-               <span>
-                  {post.tags[0]}
-               </span>
-               <span class="text-xs text-slate-500/50">
-                  +{post.tags.length - 1} more
-               </span>
-            </div>
-         {/if}
          <time>{post.date}</time>
+         <div class="tags">
+            <a href={`/blog/tags/${post.tags[0]}`} class="badge primary">
+               {post.tags[0]}
+            </a>
+            <span class="text-xs text-neutral-400">
+               +{post.tags.length - 1} more
+            </span>
+         </div>
+      </div>
+   </section>
+
+   <!-- <footer>
+         {#if post.tags}
+         {/if}
       </footer>
-   </article>
+   </article> -->
 </a>
 
 <style lang="postcss">
@@ -33,14 +37,6 @@
       @apply h-full;
    }
 
-   article {
-      /* Container */
-      @apply relative;
-
-      /* Margins */
-      @apply m-0;
-   }
-   
    section {
       /* Container */
       @apply overflow-hidden;
@@ -59,7 +55,35 @@
 
       /* Border */
       @apply rounded-lg;
-      box-shadow: inset 0rem 0rem 0.5rem rgba(0,0,0, 0.75);
+
+      /* Effects */
+      @apply hover:hover:shadow-xl;
+      transition: box-shadow 1s;
+   }
+
+   section .backdrop {
+      /* Container */
+      @apply relative flex flex-col;
+
+      /* Sizing */
+      @apply w-full h-full;
+
+      /* Alignments */
+      @apply items-center justify-center self-center;
+
+      /* Margins */
+      @apply m-auto;
+
+      /* Paddings */
+      @apply px-4 py-10;
+
+      /* Background */
+      @apply backdrop-blur-lg backdrop-brightness-50;
+
+      /* Effects */
+      @apply hover:backdrop-brightness-0 focus:backdrop-brightness-0;
+      transition: opacity 1s, background-color 1s,
+         backdrop-filter 1s;
    }
 
    section h1 {
@@ -73,26 +97,24 @@
       @apply items-center justify-center self-center;
 
       /* Margins */
-      @apply m-auto;
+      @apply m-auto my-6;
 
       /* Paddings */
-      @apply p-4;
+      /* @apply p-4; */
 
       /* Typography */
       @apply text-neutral-200 text-xl text-center;
 
-      /* Background */
-      @apply backdrop-blur-lg backdrop-brightness-50;
-      
       /* Effects */
-      transition: opacity 1s;
+      @apply hover:text-neutral-100 focus:text-neutral-100;
+      transition: opacity 1s, color 1s;
    }
 
-   article footer .tags {
-      @apply space-x-2;
+   section .tags {
+      @apply absolute bottom-1 left-2 space-x-1;
    }
 
-   article footer time {
-      @apply text-neutral-500;
+   section time {
+      @apply absolute bottom-1 right-2 text-xs text-neutral-500;
    }
 </style>
