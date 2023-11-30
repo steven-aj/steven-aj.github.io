659e77662a1226aa07fbeb77afac02189731bf19
Steven A.J <johnsonstheory@gmail.com>
2023-11-30 12:06:56 -0700

    additional favicon support

---
+++ src/components/shell/Hero.svelte
@@ -0,0 +1,45 @@
+<script>
+   import { scale } from "svelte/transition";
+
+   export let store;
+</script>
+
+{#if $store.hero}
+   <div
+      class="hero"
+      data-cover={$store.hero.cover ? "true" : "false"}
+      style={`background-image: ${
+         $store.hero.cover ? `url(${$store.hero.cover})` : ""
+      };`}
+   >
+      <div class="content">
+         <!-- {#key $store.hero} -->
+            <h1 in:scale>{$store.hero.title}</h1>
+         <!-- {/key} -->
+
+         {#if $store.hero.description}
+            <p class="description">{$store.hero.description}</p>
+         {/if}
+
+         {#if $store.hero.tags}
+            <div class="tags">
+               {#each $store.hero.tags as tag}
+                  <a
+                     class="chip variant-filled-secondary"
+                     title={tag}
+                     href={`/blog/tags/${tag}`}>{tag}</a
+                  >
+               {/each}
+            </div>
+         {/if}
+
+         {#if $store.hero.date}
+            <time>{$store.hero.date}</time>
+         {/if}
+      </div>
+   </div>
+{/if}
+
+<style>
+   
+</style>
