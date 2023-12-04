1937bdd82cb61016f059ae4b3553020becc38492
Steven A.J <johnsonstheory@gmail.com>
2023-12-04 10:47:23 -0700

    added privacy policy

--- src/lib/api/controllers/PageController.ts
+++ src/lib/api/controllers/PageController.ts
@@ -2,6 +2,7 @@ import Page from "../models/Page";
 
 export default class PageController {
    private pageEntries = Object.entries(import.meta.glob('/src/content/*.md'));
+   private legalEntries = Object.entries(import.meta.glob('/src/content/legal/*.md'));
 
    public async getAll() {
       const pages: Page[] = await Promise.all(
@@ -11,10 +12,18 @@ export default class PageController {
       return pages;
    }
 
-   public async get(path) {
-      const page: Page = (await this.getAll()).filter(page => page.path === path)[0];
+   public async getLegal() {
+      const pages: Page[] = await Promise.all(
+         this.legalEntries.map(Page.createFromPromise)
+      );
 
-      if (page) return page;
+      return pages;
+   }
+
+   public async get(path) {
+      const pages: Page[]|Page = path.includes('/legal/') ? await this.getLegal() : await this.getAll();
+
+      if (pages) return pages.filter(page => page.path === path)[0];
       else throw new Error(`Page path not found: ${path}`)
    }
 }
--- src/routes/terms-of-use/+page.js
+++
@@ -1,6 +0,0 @@
-import { Pages } from "$lib/api";
-
-export async function load() {
-   const { meta, content } = await Pages.get('terms-of-use');
-   return { meta, content };
-}
