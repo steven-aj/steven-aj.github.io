d37f3f591b407116e7250d464f54e5291b97ce58
Steven A.J. <johnsonstheory@gmail.com>
2025-07-31 15:43:36 -0500

    add remarkSlug for TOC

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,9 +1,11 @@
 import rehypeRaw from "rehype-raw"
+import remarkSlug from "remark-slug";
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexConfig = {
     extensions: [".md"],
     smartypants: { dashes: 'oldschool' },
+    remarkPlugins: [remarkSlug],
     rehypePlugins: [rehypeRaw],
     layout: {}
 }
