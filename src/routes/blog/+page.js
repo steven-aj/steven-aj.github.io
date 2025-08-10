3f96f4ea1714f0538cd7ece0517c8a2e160fd159
Steven A.J. <johnsonstheory@gmail.com>
2025-08-10 11:10:41 -0500

    exclude unnecessary files

--- src/routes/blog/+page.js
+++ src/routes/blog/+page.js
@@ -2,6 +2,7 @@ import { Posts } from "$lib/api";
 
 export async function load({ parent }) {
    // const meta = await parent();
+   // @ts-ignore
    const page = await import('$markdown/blog.md');
    // const { title, description, keywords } = page.metadata;
    
