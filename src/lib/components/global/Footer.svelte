4e9c2fec5de7c5b00c30a2ce2bf9c2fab8977860
Steven A.J. <johnsonstheory@gmail.com>
2025-08-11 12:09:18 -0500

    add social icons

--- src/lib/components/global/Footer.svelte
+++ src/lib/components/global/Footer.svelte
@@ -1,15 +1,36 @@
 <script lang="ts">
     import Censor, { cleanDocument } from "$lib/services/Censor";
+    import { Coffee, Github, HatGlasses, MessageSquare } from "@lucide/svelte";
     import { onMount } from "svelte";
     let { copyright, github } = $props();
 
-    onMount(cleanDocument)
+    onMount(cleanDocument);
 </script>
 
 <footer>
+    <div class="social">
+        <a class="icon-btn" aria-label="GitHub" target="_blank" href="https://github.com/steven-aj">
+            <Github size="24" aria-label="GitHub" color="var(--text-muted)" />
+            <span>GitHub</span>
+        </a>
+        <a class="icon-btn" aria-label="Mastodon" target="_blank" href="https://indieweb.social/@steven_aj">
+            <MessageSquare aria-label="Mastodon" size="24" color="var(--text-muted)" />
+            <span>Mastodon</span>
+        </a>
+        <a class="icon-btn" aria-label="32-Bit Cafe" target="_blank" href="https://discourse.32bit.cafe/u/steven-aj/summary">
+            <Coffee aria-label="32-Bit Cafe" size="24" color="var(--text-muted)" />
+            <span>32-Bit Cafe</span>
+        </a>
+        <a class="icon-btn" aria-label="Fedora Discussion" target="_blank" href="https://discussion.fedoraproject.org/u/steven-aj/summary">
+            <HatGlasses aria-label="Fedora Discussion" size="24" color="var(--text-muted)" />
+            <span>Fedora</span>
+        </a>
+    </div>
     <section class="baseline">
         <span class="copyright">{@html copyright}</span>
-        <span class="faded">{@html Censor.cleanProfanity("Don't steal my shit")}</span>
+        <span class="faded"
+            >{@html Censor.cleanProfanity("Don't steal my shit")}</span
+        >
     </section>
 </footer>
 
@@ -17,12 +38,34 @@
     footer {
         display: flex;
         flex-direction: column;
-        color: var(--text-main);
         padding: 2rem 1rem;
         text-align: center;
-        border-top: thin solid var(--border);
+        border-top: thin dotted var(--border);
         margin-top: 3rem;
+        gap: 1rem;
         font-family: Anta;
+        color: var(--text-muted);
+    }
+
+    .social {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        gap: 1rem;
+    }
+
+    .social a {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+        color: var(--text-muted);
+        font-size: small;
+        font-family: FallingSky;
+    }
+
+    .social a:hover,
+    .social a:active {
+        color: var(--purple);
     }
 
     img#Avatar {
@@ -60,5 +103,7 @@
     span.faded {
         font-style: italic;
         font-weight: normal;
+        font-size: small;
+        opacity: 0.5;
     }
 </style>
